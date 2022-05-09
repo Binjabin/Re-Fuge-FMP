a776d817ce90a27516db0d0541a6f3d86708aa75
Binjabin <94044545+Binjabin@users.noreply.github.com>
2022-05-09 12:44:51 +0100

    Continued work on radar system.

--- Assets/Scripts/Radar/Radar.cs
+++ Assets/Scripts/Radar/Radar.cs
@@ -8,12 +8,14 @@ public class Radar : MonoBehaviour
     [SerializeField] Transform sweepTransform;
     [SerializeField] float rotationSpeed;
     [SerializeField] float radarDistance;
-    private List<Collider> colliderList;
+    
     [SerializeField] LayerMask layers;
     [SerializeField] Transform radarCamera;
-    List<Collider> pingedColliders = new List<Collider>();
+    [SerializeField] List<Collider> pingedColliders = new List<Collider>();
 
-    
+    [SerializeField] Color importantPingColor;
+    [SerializeField] Color enemyPingColor;
+    [SerializeField] Color otherPingColor;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,30 +25,51 @@ public class Radar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float previousRotation = (transform.eulerAngles.y % 360) - 180;
+        sweepTransform.position = transform.position;
+        float previousRotation = (sweepTransform.eulerAngles.y % 360) - 180;
         sweepTransform.eulerAngles -= new Vector3(0f, (rotationSpeed * Time.deltaTime), 0f);
-        float currentRotation = (transform.eulerAngles.y % 360) - 180;
+        float currentRotation = (sweepTransform.eulerAngles.y % 360) - 180;
 
         if(previousRotation < 0 && currentRotation >= 0)
         {
             pingedColliders.Clear();
+            Debug.Log("clear list");
         }
 
-        RaycastHit ray;
-        Physics.Raycast(sweepTransform.position, sweepTransform.forward, out ray, radarDistance, layers);
+        RaycastHit[] ray = Physics.RaycastAll(sweepTransform.position, sweepTransform.forward, radarDistance, layers); ;
+        
         Debug.DrawRay(transform.position, sweepTransform.forward * radarDistance, Color.green);
-
-        if (ray.collider != null)
+        foreach(RaycastHit raycastHit in ray)
         {
-            //Instantiate(ping, ray.point, Quaternion.identity);
-            //Debug.DrawRay(transform.position, transform.forward * rayDistance, Color.yellow);
-
-            if(!pingedColliders.Contains(ray.collider))
+            if (raycastHit.collider != null)
             {
-                pingedColliders.Add(ray.collider);
-                GameObject newPing = Instantiate(radarPing, ray.collider.transform.position, Quaternion.Euler(90, 0, 0));
-                newPing.transform.parent = radarCamera.transform;
+                if(raycastHit.collider.gameObject.tag != "Player")
+                {
+                    Debug.Log(raycastHit.collider.gameObject.tag);
+                    if (!pingedColliders.Contains(raycastHit.collider))
+                    {
+                        pingedColliders.Add(raycastHit.collider);
+                        GameObject newPing = Instantiate(radarPing, raycastHit.collider.transform.position, Quaternion.Euler(90, 0, 0));
+                        if (raycastHit.collider.gameObject.GetComponent<Asteroids>() != null)
+                        {
+                            newPing.GetComponent<RadarPing>().SetColor(otherPingColor);
+                        }
+                        else if (raycastHit.collider.gameObject.GetComponent<EnemyMovement>() != null)
+                        {
+                            newPing.GetComponent<RadarPing>().SetColor(enemyPingColor);
+                        }
+                        else
+                        {
+                            newPing.GetComponent<RadarPing>().SetColor(importantPingColor);
+                        }
+                        newPing.GetComponent<RadarPing>().SetDisappearTimer(360f / rotationSpeed);
+                        //newPing.transform.parent = radarCamera.transform;
+                    }
+                }
+
+                
             }
         }
+        
     }
 }
--- Assets/Scripts/Radar/RadarPing.cs
+++ Assets/Scripts/Radar/RadarPing.cs
@@ -6,19 +6,34 @@ public class RadarPing : MonoBehaviour
 {
     float disappearTimer;
     [SerializeField] float disappearTimerMax;
+    private Color color;
+    SpriteRenderer sr;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         disappearTimer += Time.deltaTime;
-        if(disappearTimer >= disappearTimerMax)
+        color.a = Mathf.Lerp(disappearTimerMax, 0f, disappearTimer / disappearTimerMax);
+        if (disappearTimer >= disappearTimerMax)
         {
             Destroy(gameObject);
         }
+        sr.color = color;
+    }
+
+    public void SetColor(Color setColor)
+    {
+        this.color = setColor;
+    }
+
+    public void SetDisappearTimer(float disappearTimerMax)
+    {
+        this.disappearTimerMax = disappearTimerMax;
+        disappearTimer = 0f;
     }
 }
